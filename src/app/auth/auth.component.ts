0266f63a808ee5cbe2358a58b963d04c680ed180
Agam Gupta <agamgupta2895@gmail.com>
2018-01-26 04:04:53 +0530

    completed front end. auth guard left

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Subscription} from "rxjs/Subscription";
+import {HomeService} from "../home/home.service";
+import {MessageService} from "../services/message.service";
 
 @Component({
   selector: 'app-auth',
@@ -7,11 +10,24 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AuthComponent implements OnInit {
   existingUser: boolean;
+  subscription: Subscription;
+  showMessage = false;
 
-  constructor() {
+  constructor(public homeService: HomeService, public messageService: MessageService) {
   }
 
   ngOnInit() {
+    this.subscription = this.homeService.searchValue.subscribe(
+      (response) => {
+        if (response !== '') {
+          this.showMessage = true;
+          this.messageService.setHeader('Cannot search');
+          this.messageService.setMeassage('Please login before searching a user');
+        } else {
+          this.showMessage = false;
+        }
+      }
+    );
   }
 
   onSignIn() {
@@ -20,6 +36,9 @@ export class AuthComponent implements OnInit {
 
   onSignUp() {
     this.existingUser = false;
+  }
+  dismissMessage() {
+    this.showMessage = false;
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs/Subscription';
 import {HomeService} from './home.service';
 import {SearchService} from '../services/search.service';
+import {MessageService} from "../services/message.service";
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,12 @@ import {SearchService} from '../services/search.service';
 export class HomeComponent implements OnInit {
   search = false;
   users: any;
+  showMessage = false;
   private subscription: Subscription;
 
   constructor(public homeService: HomeService,
               public searchService: SearchService,
-              ) {
+              public messageService: MessageService) {
   }
 
   ngOnInit() {
@@ -25,23 +27,16 @@ export class HomeComponent implements OnInit {
         this.search = true;
         if (response === '') {
           this.search = false;
+          this.showMessage = false;
           this.getUserDetails();
         } else {
-          this.searchService.getUsers(response).subscribe(
-            (searchUsers) => {
-              console.log(searchUsers.json())
-              if (searchUsers.json().success) {
-                this.users = searchUsers.json().data;
-              } else {
-                console.log('error handling');
-              }
-            }
-          );
+          this.getSearchUsers(response);
         }
       }
     );
   }
-  getUserDetails(){
+
+  getUserDetails() {
     this.homeService.getUser(localStorage.getItem('user_id')).subscribe(
       (user) => {
         if (user.json().success) {
@@ -52,5 +47,21 @@ export class HomeComponent implements OnInit {
       }
     );
   }
+  getSearchUsers(response) {
+    this.searchService.getUsers(response).subscribe(
+      (searchUsers) => {
+        if (searchUsers.json().success) {
+          this.users = searchUsers.json().data;
+        } else {
+          this.showMessage = true;
+          this.messageService.setHeader(searchUsers.json().data.header);
+          this.messageService.setMeassage(searchUsers.json().data.message);
+        }
+      }
+    );
+  }
 
+  dismissMessage() {
+    this.showMessage = false;
+  }
 }
